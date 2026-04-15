c29d2c542cd37bb88389632d978ee2d4f43c12c4
vivekThoke <vivekthoke5@gmail.com>
2026-04-15 09:10:01 +0530

    modified main extract function

--- extractor/field_extractor.py
+++ extractor/field_extractor.py
@@ -1,30 +1,5 @@
 import re
 
-def extract_fields(text: str) -> dict:
-    data = {}
-    
-    # Policy Number
-    policy_match = re.search(r"POLICY NUMBER[:\s]*([A-Z0-9-]+)", text)
-    data["policy_number"] = policy_match.group(1) if policy_match else None
-    
-    # Name
-    name_match = re.search(r"NAME OF INSURED.*?:\s*(.+)", text)
-    data["policyholder_name"] = name_match.group(1) if name_match else None
-    
-    # Date of Loss
-    date_match = re.search(r"DATE OF LOSS.*?:\s*(\d{2}/\d{2}/\d{4})", text)
-    data["incident_date"] = date_match.group(1) if date_match else None
-    
-    # Description
-    desc_match = re.search(r"DESCRIPTION OF ACCIDENT.*?:\s*(.+)", text)
-    data["description"] = desc_match.group(1) if desc_match else None
-    
-    # Estimated Damage
-    damage_match = re.search(r"ESTIMATE AMOUNT[:\s]*([\d,]+)", text)
-    data["estimated_damage"] = int(damage_match.group(1).replace(",", "")) if damage_match else None
-    
-    return data
-
 # Policy Number
 def extract_policy_number(text):
     lines = text.split("\n")
@@ -72,3 +47,26 @@ def extract_description(text):
 
     return None
 
+
+# Estimated damage
+def extract_damage(text):
+    lines = text.split("\n")
+
+    for i, line in enumerate(lines):
+        if "ESTIMATE AMOUNT" in line.upper():
+            for j in range(i+1, i+5):
+                if j < len(lines):
+                    val = lines[j].replace(",", "").strip()
+                    if val.isdigit():
+                        return int(val)
+    return None
+
+
+def extract_fields(text):
+    return {
+        "policy_number": extract_policy_number(text),
+        "policyholder_name": extract_name(text),
+        "incident_date": extract_date(text),
+        "description": extract_description(text),
+        "estimated_damage": extract_damage(text)
+    }
