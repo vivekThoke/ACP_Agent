1a4d6dfb227d99c4310a4f6ff5c0aa3d02b35a11
vivekThoke <vivekthoke5@gmail.com>
2026-04-16 20:28:29 +0530

    added the api service for frontend

---
+++ claims-ui/src/services/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000",
+});
+
+export const processClaim = (formData) => {
+  return API.post("/process-claim", formData);
+};
